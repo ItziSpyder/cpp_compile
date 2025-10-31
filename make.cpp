120b7018ebf007cf98abb25c331a9ae7214e64da
ImproperIssues <114215797+ItziSpyder@users.noreply.github.com>
2025-10-31 03:02:05 -0700

    tweaked how the console prints out stuff idk

--- make.cpp
+++ make.cpp
@@ -37,7 +37,7 @@ int main(int argc, char* argv[]) {
 
     string target = argc == 1 ? "unnamed_application" : argv[1];
     string command = construct_command(files, target);
-    cout << "Compiling with command: " << command << endl;
+    cout << command << endl;
     system(command.c_str());
     return 0;
 }
